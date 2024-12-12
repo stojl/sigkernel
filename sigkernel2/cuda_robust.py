fa1f4ca450f8070ff08698e14e15edd87c61ae0c
Lucas <lucashillerod@hotmail.com>
2024-12-12 13:59:44 +0000

    save

--- sigkernel2/cuda_robust.py
+++ sigkernel2/cuda_robust.py
@@ -2,6 +2,108 @@ import torch.cuda
 from numba import cuda
 import math
 
+@cuda.jit(device=True)
+def cache_offset(n, m, j):
+    s = 0
+    for i in range(j):
+        s += min(n - i, m)
+    return s
+
+@cuda.jit
+def sigkernel_norms2(M_inc, C, norms, len_x, n_anti_diagonals, M_sol, M_sol2, d_order, L):
+    """
+    We start from a list of pairs of paths [(x^1,y^1), ..., (x^n, y^n)]
+    M_inc: a 3-tensor D[i,j,k] = <x^i_j, y^i_k>.
+    n_anti_diagonals = 2 * max(len_x, len_y) - 1
+    M_sol: a 3-tensor storing the solutions of the PDEs.
+    """
+
+    # Each block corresponds to a pair (x_i,y_i).
+    block_id = cuda.blockIdx.x
+    thread_id = cuda.threadIdx.x
+    
+    a = 0.0
+    b = 1.0
+    c = 0.5
+    
+    K1 = 0
+    K2 = 2
+    K3 = 1
+
+    # Bisection method
+    for _ in range(9):
+        c = 0.5 * (a + b)
+        
+        for p in range(2, n_anti_diagonals):
+            for l in range(L):
+                i = thread_id * L + l + 1
+                j = p - i
+                
+                if i < min(len_x, p) and j < len_x:
+                    inc = M_inc[block_id, (i - 1) >> d_order, (j - 1) >> d_order]
+                    
+                    k_01 = 1.0 if i == 1 else M_sol[block_id, i - 2, K2]
+                    k_10 = 1.0 if j == 1 else M_sol[block_id, i - 1, K2]
+                    k_00 = 1.0 if j == 1 or i == 1 else M_sol[block_id, i - 2, K3]
+                                
+                    M_sol[block_id, i - 1, K1] = (k_01 + k_10)*(1. + 0.5 * inc * c**2 + (1./12) * inc**2 * c**4) - k_00 * (1. - (1./12) * inc**2 * c**4)
+                    
+                    if p == n_anti_diagonals - 1:
+                        M_sol[block_id, 0, 0] = M_sol[block_id, i - 1, K1]
+
+            K1 = K1 ^ K2 ^ K3
+            K2 = K1 ^ K2 ^ K3
+            K3 = K1 ^ K2 ^ K3
+            K1 = K1 ^ K2 ^ K3
+                
+            cuda.syncthreads()
+            
+        if M_sol[block_id, 0, 0] < norms[block_id]:
+            a = c
+        else:
+            b = c
+            
+    if thread_id == 0:
+        C[block_id] = c    
+    
+    # Newton-Raphson iteration
+    """for _ in range(5):
+        for p in range(2, n_anti_diagonals):
+            for l in range(L):
+                i = thread_id * L + l + 1
+                j = p - i
+                
+                if i < min(len_x, p) and j < len_x:
+                    inc = M_inc[block_id, (i - 1) >> d_order, (j - 1) >> d_order]
+                    
+                    k_01 = 1.0 if i == 1 else M_sol[block_id, i - 2, K2]
+                    k_10 = 1.0 if j == 1 else M_sol[block_id, i - 1, K2]
+                    k_00 = 1.0 if j == 1 or i == 1 else M_sol[block_id, i - 2, K3]
+                    
+                    k_01_2 = 0.0 if i == 1 else M_sol2[block_id, i - 2, K2]
+                    k_10_2 = 0.0 if j == 1 else M_sol2[block_id, i - 1, K2]
+                    k_00_2 = 0.0 if j == 1 or i == 1 else M_sol2[block_id, i - 2, K3]
+                                
+                    M_sol[block_id, i - 1, K1] = (k_01 + k_10)*(1. + 0.5 * inc * c**2 + (1./12) * inc**2 * c**4) - k_00 * (1. - (1./12) * inc**2 * c**4)
+                    M_sol2[block_id, i - 1, K1] = (k_01_2 + k_10_2) * (1. + inc * c**2 + (1./12) * inc**2 * c**4) - k_00_2 * (1. - (1./12) * inc**2 * c**4)
+                    M_sol2[block_id, i - 1, K1] += 0.5 * inc * c * (k_01 + k_10 + k_00 + M_sol[block_id, i - 1, K1])
+                    
+                    if p == n_anti_diagonals - 1:
+                        M_sol[block_id, 0, 0] = M_sol[block_id, i - 1, K1]
+                        M_sol2[block_id, 0, 0] = M_sol2[block_id, i - 1, K1]
+
+            K1 = K1 ^ K2 ^ K3
+            K2 = K1 ^ K2 ^ K3
+            K3 = K1 ^ K2 ^ K3
+            K1 = K1 ^ K2 ^ K3
+                
+            cuda.syncthreads()
+                
+        if thread_id == 0:
+            C[block_id] = c - (M_sol[block_id, 0, 0] - norms[block_id]) / M_sol2[block_id, 0, 0]
+            c = C[block_id]
+            
+        cuda.syncthreads()"""
 
 @cuda.jit
 def sigkernel_norms(M_inc, C, norms, len_x, n_anti_diagonals, M_sol, M_sol2, d_order, L, N, rel_tol, abs_tol):
@@ -111,4 +213,93 @@ def robust_sigkernel(M_inc, x_norm, y_norm, len_x, n_anti_diagonals, M_sol, d_or
         K3 = K1 ^ K2 ^ K3
         K1 = K1 ^ K2 ^ K3
             
+        cuda.syncthreads()
+        
+        
+@cuda.jit
+def robust_sigkernel_gram(M_inc, x_norm, y_norm, len_x, len_y, n_anti_diagonals, M_sol, d_order, L):
+    """
+    We start from a list of pairs of paths [(x^1,y^1), ..., (x^n, y^n)]
+    M_inc: a 3-tensor D[i,j,k] = <x^i_j, y^i_k>.
+    n_anti_diagonals = 2 * max(len_x, len_y) - 1
+    M_sol: a 3-tensor storing the solutions of the PDEs.
+    """
+
+    # Each block corresponds to a pair (x_i,y_i).
+    block_x = cuda.blockIdx.x
+    block_y = cuda.blockIdx.y
+    thread_id = cuda.threadIdx.x
+    
+    K1 = 0
+    K2 = 2
+    K3 = 1
+
+    # Go over each anti-diagonal. Only process threads that fall on the current on the anti-diagonal
+    for p in range(2, n_anti_diagonals):
+        for l in range(L):
+            i = thread_id * L + l + 1
+            j = p - i
+            
+            if i < min(len_x, p) and j < len_y:
+                inc = M_inc[block_x, block_y, (i - 1) >> d_order, (j - 1) >> d_order]
+                
+                k_01 = 1.0 if i == 1 else M_sol[block_x, block_y, i - 2, K2]
+                k_10 = 1.0 if j == 1 else M_sol[block_x, block_y, i - 1, K2]
+                k_00 = 1.0 if j == 1 or i == 1 else M_sol[block_x, block_y, i - 2, K3]
+                               
+                M_sol[block_x, block_y, i - 1, K1] = (k_01 + k_10) * (1. + 0.5 * inc * x_norm[block_x] * y_norm[block_y] + (1./12) * inc**2 * x_norm[block_x]**2 * y_norm[block_y]**2) - k_00 * (1. - (1./12) * inc**2 * x_norm[block_x]**2 * y_norm[block_y]**2)
+                
+                if p == n_anti_diagonals - 1:
+                    M_sol[block_x, block_y, 0, 0] = M_sol[block_x, block_y, i - 1, K1]
+
+        K1 = K1 ^ K2 ^ K3
+        K2 = K1 ^ K2 ^ K3
+        K3 = K1 ^ K2 ^ K3
+        K1 = K1 ^ K2 ^ K3
+            
+        cuda.syncthreads()
+        
+        
+@cuda.jit
+def robust_sigkernel_gram_sym(M_inc, W, Out, norms, len_x, len_y, x_off, y_off, M_grid, N_grid, n_anti_diagonals, d_order, L):
+    block_x = cuda.blockIdx.x
+    block_y = cuda.blockIdx.y
+    thread_id = cuda.threadIdx.x
+    
+    if block_y + y_off > block_x + x_off:
+        return
+    
+    c_off = cache_offset(N_grid, M_grid, block_y)
+    cx = M_grid - min(N_grid - (block_y + y_off), M_grid)
+    W = W[c_off + block_x - cx,:,:]
+    
+    K1 = 0
+    K2 = 2
+    K3 = 1
+
+    # Go over each anti-diagonal. Only process threads that fall on the current on the anti-diagonal
+    for p in range(2, n_anti_diagonals):
+        for l in range(L):
+            i = thread_id * L + l + 1
+            j = p - i
+            
+            if i < min(len_x, p) and j < len_y:
+                inc = M_inc[block_x, block_y, (i - 1) >> d_order, (j - 1) >> d_order]
+                
+                k_01 = 1.0 if i == 1 else W[i - 2, K2]
+                k_10 = 1.0 if j == 1 else W[i - 1, K2]
+                k_00 = 1.0 if j == 1 or i == 1 else W[i - 2, K3]
+                               
+                W[i - 1, K1] = (k_01 + k_10) * (1. + 0.5 * inc * norms[block_x + x_off] * norms[block_y + y_off] + (1./12) * inc**2 * norms[block_x + x_off]**2 * norms[block_y + y_off]**2) - k_00 * (1. - (1./12) * inc**2 * norms[block_x + x_off]**2 * norms[block_y + y_off]**2)
+                
+                if p == n_anti_diagonals - 1:
+                    Out[block_x + x_off, block_y + y_off] = W[i - 1, K1]
+                    if block_x + x_off != block_y + y_off:
+                        Out[block_y + y_off, block_x + x_off] = W[i - 1, K1]
+
+        K1 = K1 ^ K2 ^ K3
+        K2 = K1 ^ K2 ^ K3
+        K3 = K1 ^ K2 ^ K3
+        K1 = K1 ^ K2 ^ K3
+        
         cuda.syncthreads()
--- sigkernel2/sigkernel2.py
+++ sigkernel2/sigkernel2.py
@@ -2,7 +2,7 @@ import torch
 import torch.cuda
 from numba import cuda
 from .cuda_backend2 import sigkernel_cuda2, sigkernel_gram_cuda2, sigkernel_gram_sym_cuda2
-from .cuda_robust import sigkernel_norms, robust_sigkernel
+from .cuda_robust import sigkernel_norms, robust_sigkernel, sigkernel_norms2, robust_sigkernel_gram, robust_sigkernel_gram_sym
 
 def ceil_div(a, b):
     return -(-a // b)
@@ -42,7 +42,7 @@ class RobustSigKernel():
         G_static_ = G_static[:, :, 1:, 1:] + G_static[:, :, :-1, :-1] - G_static[:, :, 1:, :-1] - G_static[:, :, :-1, 1:]
         return G_static_ / float(2**(2 * self.dyadic_order))
     
-    def norms(self, X, maxit=100, rel_tol=1e-4, abs_tol=1e-4, max_batch=100, max_threads=1024):
+    def norms(self, X, max_batch=100, max_threads=1024):
         batch_size = X.shape[0]
         M = X.shape[1]
         
@@ -59,6 +59,7 @@ class RobustSigKernel():
         W = torch.zeros([mb_size, MM - 1, 3], device=X.device, dtype=X.dtype)
         W2 = torch.zeros([mb_size, MM - 1, 3], device=X.device, dtype=X.dtype)
         K = torch.zeros([batch_size], device=X.device, dtype=X.dtype)
+        norm_out = torch.zeros([batch_size], device=X.device, dtype=X.dtype)
         norm_const = torch.zeros([batch_size], device=X.device, dtype=X.dtype)
 
         for i in range(bm):
@@ -82,27 +83,23 @@ class RobustSigKernel():
             
             K[start:stop] = W[0:mb_size_i,0,0]
             K_norm = self.normalizer(K[start:stop])
-            norm_guess = self.guess_norm(K[start:stop], K_norm)
             
-            sigkernel_norms[mb_size_i, threads_per_block](
+            sigkernel_norms2[mb_size_i, threads_per_block](
                 cuda.as_cuda_array(inc.detach()),
-                cuda.as_cuda_array(norm_guess.detach()),
+                cuda.as_cuda_array(norm_out),
                 cuda.as_cuda_array(K_norm.detach()),
                 MM, n_anti_diagonals,
                 cuda.as_cuda_array(W),
                 cuda.as_cuda_array(W2),
-                self.dyadic_order, L,
-                maxit,
-                rel_tol,
-                abs_tol
+                self.dyadic_order, L
             )
             
             cuda.synchronize()
-            norm_const[start:stop] = norm_guess
+            norm_const[start:stop] = norm_out
 
         return norm_const
 
-    def kernel(self, X, Y, maxit=100, rel_tol=1e-4, abs_tol=1e-4, max_batch=100, max_threads=1024, y_norms=None, x_norms=None):
+    def kernel(self, X, Y, max_batch=100, max_threads=1024, y_norms=None, x_norms=None):
         batch_size = X.shape[0]
         M = X.shape[1]
         N = Y.shape[1]
@@ -121,8 +118,8 @@ class RobustSigKernel():
         W = torch.zeros([mb_size, MM - 1, 3], device=X.device, dtype=X.dtype)
         K = torch.zeros([batch_size], device=X.device, dtype=X.dtype)
         
-        X_norms = self.norms(X, maxit=maxit, rel_tol=rel_tol, abs_tol=abs_tol, max_batch=max_batch, max_threads=max_threads) if x_norms is None else x_norms 
-        Y_norms = self.norms(Y, maxit=maxit, rel_tol=rel_tol, abs_tol=abs_tol, max_batch=max_batch, max_threads=max_threads) if y_norms is None else y_norms
+        X_norms = self.norms(X, max_batch=max_batch, max_threads=max_threads) if x_norms is None else x_norms 
+        Y_norms = self.norms(Y, max_batch=max_batch, max_threads=max_threads) if y_norms is None else y_norms
         
         for i in range(bm):
             mb_size_i = batch_size - mb_size * (bm - 1) if i == bm - 1 else mb_size
@@ -149,6 +146,119 @@ class RobustSigKernel():
             
         return K
     
+    def gram(self, X, Y=None, max_batch=100, max_threads=1024):
+        if Y is None:
+            return self._gram_sym(X, max_batch, max_threads)
+        else:
+            return self._gram(X, Y, max_batch, max_threads)
+    
+    def _gram(self, X, Y, max_batch=10, max_threads=1024):
+        batch_x = X.shape[0]
+        batch_y = Y.shape[0]
+        M = X.shape[1]
+        N = Y.shape[1]
+
+        MM = (2**self.dyadic_order)*(M-1) + 1
+        NN = (2**self.dyadic_order)*(N-1) + 1
+        n_anti_diagonals = MM + NN - 1
+        
+        threads_per_block = min(max_threads, MM - 1, 1024)
+        threads_per_block = round_to_multiple_of_32(threads_per_block)
+        L = -(-(MM - 1) // threads_per_block)
+        
+        bm_x = ceil_div(batch_x, max_batch)
+        bm_y = ceil_div(batch_y, max_batch)
+        
+        mb_size_x = min(batch_x, max_batch)
+        mb_size_y = min(batch_y, max_batch)
+        
+        W = torch.zeros([mb_size_x, mb_size_y, MM - 1, 3], device=X.device, dtype=X.dtype)
+        K = torch.zeros([batch_x, batch_y], device=X.device, dtype=X.dtype)
+        
+        X_norms = self.norms(X, max_batch=max_batch, max_threads=max_threads)
+        Y_norms = self.norms(Y, max_batch=max_batch, max_threads=max_threads)
+        
+        for j in range(bm_y):
+            mb_j = batch_y - mb_size_y * (bm_y - 1) if j == bm_y - 1 else mb_size_y
+            start_j = j * mb_size_y
+            stop_j = j * mb_size_y + mb_j
+            for i in range(bm_x):
+                mb_i = batch_x - mb_size_x * (bm_x - 1) if i == bm_x - 1 else mb_size_x
+                
+                start_i = i * mb_size_x
+                stop_i = i * mb_size_x + mb_i
+                
+                inc = self.dist_gram(
+                    X[start_i:stop_i,:,:],
+                    Y[start_j:stop_j,:,:]
+                )
+
+                robust_sigkernel_gram[(mb_i, mb_j), threads_per_block](
+                    cuda.as_cuda_array(inc.detach()),
+                    cuda.as_cuda_array(X_norms[start_i:stop_i]),
+                    cuda.as_cuda_array(Y_norms[start_j:stop_j]),
+                    MM, NN, n_anti_diagonals,
+                    cuda.as_cuda_array(W), 
+                    self.dyadic_order, L
+                )
+                
+                cuda.synchronize()
+                
+                K[start_i:stop_i, start_j:stop_j] = W[0:mb_i, 0:mb_j, 0, 0]
+                
+        return K
+    
+    def _gram_sym(self, X, max_batch=10, max_threads=1024):
+        batch_size = X.shape[0]
+        M = X.shape[1]
+
+        MM = (2**self.dyadic_order)*(M-1) + 1
+        n_anti_diagonals = MM + MM - 1
+        
+        threads_per_block = min(max_threads, MM - 1, 1024)
+        threads_per_block = round_to_multiple_of_32(threads_per_block)
+        L = -(-(MM - 1) // threads_per_block)
+        
+        bm = ceil_div(batch_size, max_batch)
+        mb_size = min(batch_size, max_batch)
+        
+        tri_cache_size = triangular_cache_size(batch_size, mb_size)
+        
+        W = torch.zeros([tri_cache_size, MM - 1, 3], device=X.device, dtype=X.dtype)
+        K = torch.zeros([batch_size, batch_size], device=X.device, dtype=X.dtype)
+        
+        X_norms = self.norms(X, max_batch=max_batch, max_threads=max_threads)
+        
+        for j in range(bm):
+            mb_j = batch_size - mb_size * (bm - 1) if j == bm - 1 else mb_size
+            start_j = j * mb_size
+            stop_j = j * mb_size + mb_j
+            for i in range(bm):
+                mb_i = batch_size - mb_size * (bm - 1) if i == bm - 1 else mb_size
+                start_i = i * mb_size
+                stop_i = i * mb_size + mb_i
+                
+                inc = self.dist_gram(
+                    X[start_i:stop_i,:,:],
+                    X[start_j:stop_j,:,:]
+                )
+                
+                robust_sigkernel_gram_sym[(mb_i, mb_j), threads_per_block](
+                    cuda.as_cuda_array(inc.detach()),
+                    cuda.as_cuda_array(W),
+                    cuda.as_cuda_array(K),
+                    cuda.as_cuda_array(X_norms),
+                    MM, MM, 
+                    i * mb_size, j * mb_size,
+                    mb_size, batch_size,
+                    n_anti_diagonals,
+                    self.dyadic_order, 
+                    L
+                )
+                
+                cuda.synchronize()
+        return K
+    
 
 class SigKernel2():
     def __init__(self, static_kernel, dyadic_order):
@@ -164,54 +274,6 @@ class SigKernel2():
         G_static = self.static_kernel.Gram_matrix(X, Y)
         G_static_ = G_static[:, :, 1:, 1:] + G_static[:, :, :-1, :-1] - G_static[:, :, 1:, :-1] - G_static[:, :, :-1, 1:]
         return G_static_ / float(2**(2 * self.dyadic_order))
-    
-    def robust_kernel(self, X, norms, C, n=10, rel_tol=1e-4, abs_tol=1e-4, max_batch=100, max_threads=1024):
-        batch_size = X.shape[0]
-        M = X.shape[1]
-        
-        bm = ceil_div(batch_size, max_batch)
-
-        MM = (2**self.dyadic_order)*(M-1) + 1
-        n_anti_diagonals = 2 * MM - 1
-        
-        threads_per_block = min(max_threads, MM - 1, 1024)
-        threads_per_block = round_to_multiple_of_32(threads_per_block)
-        L = -(-(MM - 1) // threads_per_block)
-        
-        mb_size = min(batch_size, max_batch)
-        W = torch.zeros([mb_size, MM - 1, 3], device=X.device, dtype=X.dtype)
-        W2 = torch.zeros([mb_size, MM - 1, 3], device=X.device, dtype=X.dtype)
-        K = torch.zeros([batch_size], device=X.device, dtype=X.dtype)
-        K2 = torch.zeros([batch_size], device=X.device, dtype=X.dtype)
-        for i in range(bm):
-            mb_size_i = batch_size - mb_size * (bm - 1) if i == bm - 1 else mb_size
-            start = i * mb_size
-            stop = i * mb_size + mb_size_i
-            
-            inc = self.dist(
-                X[start:stop,:,:],
-                X[start:stop,:,:]
-                )
-            
-            sigkernel_norms[mb_size_i, threads_per_block](
-                cuda.as_cuda_array(inc.detach()),
-                cuda.as_cuda_array(C),
-                cuda.as_cuda_array(norms),
-                MM, n_anti_diagonals,
-                cuda.as_cuda_array(W),
-                cuda.as_cuda_array(W2),
-                self.dyadic_order, L,
-                n,
-                rel_tol,
-                abs_tol
-            )
-            
-            cuda.synchronize()
-            
-            K[start:stop] = W[0:mb_size_i,0,0]
-            K2[start:stop] = W2[0:mb_size_i,0,0]
-            
-        return K, K2, C
         
     def kernel(self, X, Y, max_batch=100, max_threads=1024):
         batch_size = X.shape[0]
@@ -358,7 +420,7 @@ class SigKernel2():
                 )
                 
                 cuda.synchronize()
-        return K       
+        return K
                 
 def CHSIC(X, Y, Z, eps=0.1):
     device = X.device
